3075fbc784547d607c57ee3320b7cf1a8fc88a70
Nathan Annareddy <nathan.annareddy@gmail.com>
2023-11-08 10:02:59 -0600

    made column and row board representations

--- Board.java
+++ Board.java
@@ -1,16 +1,77 @@
+import java.lang.StringBuilder;
+import java.util.Arrays;
+
 public class Board{
-	int[] raw_board = new int[81];
+	private BoardEntry[] rawBoard;
+	private BoardEntry[][] columnBoard;
+	private BoardEntry[][] rowBoard;
+	private BoardEntry[][] squareBoard;
 
-	int[][] vert_board = new int[9][9];
-	int[][] horz_board = new int[9][9];
-	int[][] sqaure_board = new int[9][9];
+	Board(int[] inputBoard){
+		this.rawBoard = new BoardEntry[81];
+		this.columnBoard = new BoardEntry[9][9];
+		this.rowBoard = new BoardEntry[9][9];
+		this.squareBoard = new BoardEntry[9][9];
 
-	Board(int[] raw_board){
-		this.raw_board = raw_board;
-	}	
+		for(int i = 0; i < inputBoard.length; i++){
+			this.rawBoard[i] = new BoardEntry(inputBoard[i]);
+		}
 
+		makeColumnBoard();
+		makeRowBoard();
+	}
+
+	private void makeColumnBoard(){
+		for(int i = 0; i < 9; i++){
+			for(int j = 0; j < 9; j++){
+				this.columnBoard[i][j] = this.rawBoard[i+j*9];
+			}	
+		}
+	}
+
+	private void makeRowBoard(){
+		for(int i = 0; i < 9; i++){
+			for(int j = 0; j < 9; j++){
+				this.rowBoard[i][j] = this.rawBoard[i*9+j];
+			}
+		}
+	}
+
+	@Override
+	public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 81; i += 9) {
+            BoardEntry[] row = Arrays.copyOfRange(this.rawBoard, i, i + 9);
+
+            for (int j = 0; j < 9; j++) {
+				String appendValue = row[j].getValue() == -1 ? "*" : String.valueOf(row[j].getValue());
+				sb.append(appendValue);
+				sb.append(" ");
+            }
+			
+			if(i < 72){
+				sb.append("\n");
+			}		
+		}
+
+        return sb.toString();
+    }
 
 	public static void main(String[] args){
-		System.
+		int[] testBoard = {
+    		5, 3, -1, -1, 7, -1, -1, -1, -1,
+    		6, -1, -1, 1, 9, 5, -1, -1, -1,
+    		-1, 9, 8, -1, -1, -1, -1, 6, -1,
+    		8, -1, -1, -1, 6, -1, -1, -1, 3,
+    		4, -1, -1, 8, -1, 3, -1, -1, 1,
+    		7, -1, -1, -1, 2, -1, -1, -1, 6,
+			-1, 6, -1, -1, -1, -1, 2, 8, -1,
+			-1, -1, -1, 4, 1, 9, -1, -1, 5,
+	    	-1, -1, -1, -1, 8, -1, -1, 7, 9
+		};
+
+		Board myBoard = new Board(testBoard);
+		System.out.println(myBoard);
 	}
 }
--- BoardEntry.java
+++ BoardEntry.java
@@ -1,4 +1,25 @@
 public class BoardEntry{
-	boolean discovered
-	int[] potential = new int[9];
+	boolean isDiscovered;
+	int[] potentialValues;
+	int entryValue;		
+
+	public BoardEntry(){
+		this(-1);
+	}
+
+	public BoardEntry(int inputValue){
+		this.isDiscovered = true;
+		this.potentialValues = new int[9];
+		this.entryValue = inputValue;
+	}
+
+	public void setValue(int inputValue){
+		this.isDiscovered = true;
+		this.entryValue = inputValue;
+		
+		return;
+	}
+
+	public int getValue(){ return this.entryValue; }
+
 }
