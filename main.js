2e74a7074a5f784f7c77ee0d328b667f5bc97e70
Nathan Annareddy <nathan.annareddy@gmail.com>
2024-03-15 14:52:34 -0500

    refactor validateBoard()

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 function validateBoard(){
 	for(let i = 0; i < 9; i++){
 		const map = new Map();
-		Array.from({length: 9}, (_, i) => map.set(i + 1, 0));
 		for(let j = 0; j < 9; j++){
-			const value = Number(document.getElementById(String(i)+String(j)).value);
-			if(value === NaN || value < 1 || value > 9 || map.get(value) != 0){
+			const value = Number(document.getElementById(`${i}${j}`).value);
+			if(value === NaN || value < 1 || value > 9 || map.get(value) !== undefined){
 				return false;
 			}
 			map.set(value, 1);
@@ -13,10 +12,9 @@ function validateBoard(){
 
 	for(let i = 0; i < 9; i++){
 		const map = new Map();
-		Array.from({length: 9}, (_, i) => map.set(i + 1, 0));
 		for(let j = 0; j < 9; j++){
-			const value = Number(document.getElementById(String(j)+String(i)).value);
-			if(value === NaN || value < 1 || value > 9 || map.get(value) != 0){
+			const value = Number(document.getElementById(`${j}${i}`).value);
+			if(value === NaN || value < 1 || value > 9 || map.get(value) !== undefined){
 				return false;
 			}
 			map.set(value, 1);
@@ -37,6 +35,6 @@ function validateBoard(){
 			}
 		}
 	}
-	
+
 	return true;
 }
