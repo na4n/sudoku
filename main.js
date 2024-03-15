7e7fd5d939e0b75d2a16dff0dd55d5c58b23b7a1
Nathan Annareddy <nathan.annareddy@gmail.com>
2024-03-15 15:52:47 -0500

    combine row, column, valid check

--- main.js
+++ main.js
@@ -1,29 +1,24 @@
 function validateBoard() {
-    for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < 9; j++) {
-            const value = Number(document.getElementById(`${i}${j}`).value);
-            if (isNaN(value) || value < 1 || value > 9 || value === undefined) {
-                return false;
-            }
-        }
-    }
-
-    for (let i = 0; i < 9; i++) {
-        const mapRow = new Map();
-        const mapColumn = new Map();
-        for (let j = 0; j < 9; j++) {
-            const valueRow = Number(document.getElementById(`${i}${j}`).value);
-            const valueColumn = Number(document.getElementById(`${j}${i}`).value);
-
-            if ((mapRow.has(valueRow) && valueRow !== undefined) || (mapColumn.has(valueColumn) && valueColumn !== undefined)) {
-                return false;
-            }
-
-            mapRow.set(valueRow, 1);
-            mapColumn.set(valueColumn, 1);
-        }
-    }
-
+	for (let i = 0; i < 9; i++) {
+		const mapRow = new Map();
+		const mapColumn = new Map();
+		for (let j = 0; j < 9; j++) {
+			const valueRow = Number(document.getElementById(`${i}${j}`).value);
+			const valueColumn = Number(document.getElementById(`${j}${i}`).value);
+	
+			if (isNaN(valueRow) || isNaN(valueColumn) || valueRow < 1 || valueRow > 9 || valueColumn < 1 || valueColumn > 9) {
+				return false;
+			}
+	
+			if ((mapRow.has(valueRow)) || (mapColumn.has(valueColumn))) {
+				return false;
+			}
+	
+			mapRow.set(valueRow, 1);
+			mapColumn.set(valueColumn, 1);
+		}
+	}
+	
     for (let i = 0; i < 9; i += 3) {
         for (let j = 0; j < 9; j += 3) {
             const subgridMap = new Map();
