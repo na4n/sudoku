c5679ad663897021e20d14e66c9bcd9a97f5f8d1
Nathan Annareddy <nathan.annareddy@gmail.com>
2024-03-15 15:23:59 -0500

    refactor code

--- main.js
+++ main.js
@@ -1,40 +1,43 @@
-function validateBoard(){
-	for(let i = 0; i < 9; i++){
-		const map = new Map();
-		for(let j = 0; j < 9; j++){
-			const value = Number(document.getElementById(`${i}${j}`).value);
-			if(value === NaN || value < 1 || value > 9 || map.get(value) !== undefined){
-				return false;
-			}
-			map.set(value, 1);
-		}
-	}	
+function validateBoard() {
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            const value = Number(document.getElementById(`${i}${j}`).value);
+            if (isNaN(value) || value < 1 || value > 9 || value === undefined) {
+                return false;
+            }
+        }
+    }
 
-	for(let i = 0; i < 9; i++){
-		const map = new Map();
-		for(let j = 0; j < 9; j++){
-			const value = Number(document.getElementById(`${j}${i}`).value);
-			if(map.get(value) !== undefined){
-				return false;
-			}
-			map.set(value, 1);
-		}
-	}
-	
-	for (let i = 0; i < 9; i += 3) {
-		for (let j = 0; j < 9; j += 3) {
-			const map = new Map();
-			for (let x = 0; x < 3; x++) {
-				for (let y = 0; y < 3; y++) {
-					const value = Number(document.getElementById(`${i + x}${j + y}`).value);
-					if (isNaN(value) || value < 1 || value > 9 || map.get(value) !== undefined) {
-						return false;
-					}
-					map.set(value, 1);
-				}
-			}
-		}
-	}
+    for (let i = 0; i < 9; i++) {
+        const mapRow = new Map();
+        const mapColumn = new Map();
+        for (let j = 0; j < 9; j++) {
+            const valueRow = Number(document.getElementById(`${i}${j}`).value);
+            const valueColumn = Number(document.getElementById(`${j}${i}`).value);
 
-	return true;
+            if ((mapRow.has(valueRow) && valueRow !== undefined) || (mapColumn.has(valueColumn) && valueColumn !== undefined)) {
+                return false;
+            }
+
+            mapRow.set(valueRow, 1);
+            mapColumn.set(valueColumn, 1);
+        }
+    }
+
+    for (let i = 0; i < 9; i += 3) {
+        for (let j = 0; j < 9; j += 3) {
+            const subgridMap = new Map();
+            for (let x = 0; x < 3; x++) {
+                for (let y = 0; y < 3; y++) {
+                    const valueSubgrid = Number(document.getElementById(`${i + x}${j + y}`).value);
+                    if (subgridMap.has(valueSubgrid)) {
+                        return false;
+                    }
+                    subgridMap.set(valueSubgrid, 1);
+                }
+            }
+        }
+    }
+
+    return true;
 }
