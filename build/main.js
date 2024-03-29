37c0687e82fc8735048c77d2b4636f9a38a452ae
Nathan Annareddy <nathan.annareddy@gmail.com>
2024-03-29 12:32:25 -0500

    begin refactor potentialValue()

--- build/main.js
+++ build/main.js
@@ -22,7 +22,7 @@ function gridIndex(i, j) {
     return ((Math.floor(i / 3)) * 3) + (Math.floor(j / 3));
 }
 function validateBoard() {
-    if (board.length != 9) {
+    if (board.length != 9 && board[0].length != 9) {
         return false;
     }
     const mapGrid = new Map(Array.from({ length: 9 }, (_, i) => [i, new Map()]));
@@ -53,59 +53,116 @@ function solve() {
 }
 const potentialValue = new Map();
 function findPotentialValues() {
-    for (let i = 0; i < 9; i++) {
-        const arr = [];
-        let hashmap = new Map(Array.from({ length: 9 }, (_, i) => [i + 1, 1]));
-        for (let j = 0; j < 9; j++) {
-            const rowEle = document.getElementById(`${i}${j}`);
-            if ((rowEle).value === '') {
-                arr.push(`${i}${j}`);
-            }
-            else {
-                hashmap.set(Number((rowEle).value), 0);
-            }
-        }
-        const values = [];
-        for (let i = 1; i < 10; i++) {
-            if (hashmap.get(i) === 1) {
-                values.push(i);
-            }
-        }
-        for (let i = 0; i < arr.length; i++) {
-            if (potentialValue.get(arr[i]) === undefined) {
-                potentialValue.set(arr[i], values);
-            }
-            else {
-                potentialValue.set(arr[i], potentialValue.get(arr[i]).concat(values));
-            }
-        }
-    }
-    for (let j = 0; j < 9; j++) {
-        const arr = [];
-        let hashmap = new Map(Array.from({ length: 9 }, (_, i) => [i + 1, 1]));
+    function mapComplement(foundVals) {
+        const comp = [];
         for (let i = 0; i < 9; i++) {
-            const rowEle = document.getElementById(`${i}${j}`);
-            if (rowEle.value === '') {
-                arr.push(`${i}${j}`);
-            }
-            else {
-                hashmap.set(Number(rowEle.value), 0);
+            if (foundVals.get(i) === undefined) {
+                comp.push(i);
             }
         }
-        const values = [];
-        for (let i = 1; i < 10; i++) {
-            if (hashmap.get(i) === 1) {
-                values.push(i);
+        return comp;
+    }
+    function innerJoinSet(arr1, arr2) {
+        const inBoth = [];
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr2.includes(arr1[i]) && !(inBoth.includes(arr1[i]))) {
+                inBoth.push(arr1[i]);
             }
         }
-        for (let i = 0; i < arr.length; i++) {
-            if (potentialValue.get(arr[i]) === undefined) {
-                potentialValue.set(arr[i], values);
+        return inBoth;
+    }
+    if (board.length != 9 && board[0].length != 9) {
+        return false;
+    }
+    for (let i = 0; i < 9; i++) {
+        const rowPossibilities = new Map(Array.from({ length: 9 }, (_, i) => [i + 1, 1]));
+        const rowUnknowns = [];
+        const colPossibilities = new Map(Array.from({ length: 9 }, (_, i) => [i + 1, 1]));
+        const colUnknowns = [];
+        for (let j = 0; j < 9; j++) {
+            if (board[i][j] == -1) {
+                rowUnknowns.push([i, j]);
             }
             else {
-                potentialValue.set(arr[i], Array.from(new Set(potentialValue.get(arr[i]).concat(values))));
+                rowPossibilities.set(board[i][j], 0);
+            }
+            if (board[j][i] == -1) {
+                colUnknowns.push([j, i]);
+            }
+            else {
+                colPossibilities.set(board[j][i], 0);
+            }
+        }
+        for (let i = 0; i < rowUnknowns.length; i++) {
+            if (potentialValue.get(JSON.stringify(rowUnknowns[i])) === undefined) {
+                potentialValue.set(JSON.stringify(rowUnknowns[i]), mapComplement(rowPossibilities));
+            }
+            else {
+                potentialValue.set(JSON.stringify(rowUnknowns[i]), innerJoinSet(potentialValue.get(JSON.stringify(rowUnknowns[i])), mapComplement(rowPossibilities)));
+            }
+        }
+        for (let i = 0; i < colUnknowns.length; i++) {
+            if (potentialValue.get(JSON.stringify(colUnknowns[i])) === undefined) {
+                potentialValue.set(JSON.stringify(colUnknowns[i]), mapComplement(colPossibilities));
+            }
+            else {
+                potentialValue.set(JSON.stringify(colUnknowns[i]), innerJoinSet(potentialValue.get(JSON.stringify(colUnknowns[i])), mapComplement(colPossibilities)));
             }
         }
     }
+    // for (let i = 0; i < 9; i++) {
+    //     const arr: string[] = [];
+    //     let hashmap: Map<number, number> = new Map(Array.from({ length: 9 }, (_, i) => [i + 1, 1]));
+    //     for (let j = 0; j < 9; j++) {
+    //         const rowEle: HTMLElement = document.getElementById(`${i}${j}`);
+    //         if ((<HTMLInputElement>(rowEle)).value === '') {
+    //             arr.push(`${i}${j}`);
+    //         }
+    //         else {
+    //             hashmap.set(Number((<HTMLInputElement>(rowEle)).value), 0);
+    //         }
+    //     }
+    //     const values: number[] = [];
+    //     for (let i = 1; i < 10; i++) {
+    //         if (hashmap.get(i) === 1) {
+    //             values.push(i);
+    //         }
+    //     }
+    //     for (let i = 0; i < arr.length; i++) {
+    //         if (potentialValue.get(arr[i]) === undefined) {
+    //             potentialValue.set(arr[i], values);
+    //         }
+    //         else {
+    //             potentialValue.set(arr[i], potentialValue.get(arr[i]).concat(values));
+    //         }
+    //     }
+    // }
+    // for (let j = 0; j < 9; j++) {
+    //     const arr: string[] = [];
+    //     let hashmap: Map<number, number> = new Map(Array.from({ length: 9 }, (_, i) => [i + 1, 1]));
+    //     for (let i = 0; i < 9; i++) {
+    //         const rowEle = document.getElementById(`${i}${j}`);
+    //         if ((<HTMLInputElement>rowEle).value === '') {
+    //             arr.push(`${i}${j}`);
+    //         }
+    //         else {
+    //             hashmap.set(Number((<HTMLInputElement>rowEle).value), 0)
+    //         }
+    //     }
+    //     const values: number[] = [];
+    //     for (let i = 1; i < 10; i++) {
+    //         if (hashmap.get(i) === 1) {
+    //             values.push(i);
+    //         }
+    //     }
+    //     for (let i = 0; i < arr.length; i++) {
+    //         if (potentialValue.get(arr[i]) === undefined) {
+    //             potentialValue.set(arr[i], values);
+    //         }
+    //         else {
+    //             potentialValue.set(arr[i], Array.from(new Set(potentialValue.get(arr[i]).concat(values))));
+    //         }
+    //     }
+    // }
 }
 //# sourceMappingURL=main.js.map
--- src/js/main.ts
+++ src/js/main.ts
@@ -27,7 +27,7 @@ function gridIndex(i: number, j: number): number{
 }
 
 function validateBoard(): boolean {
-    if(board.length != 9){
+    if(board.length != 9 && board[0].length != 9){
         return false;
     }
 
@@ -61,65 +61,126 @@ function solve(): boolean{
 }
 
 const potentialValue: Map<string, number[]> = new Map();
-function findPotentialValues() {
-    for (let i = 0; i < 9; i++) {
-        const arr: string[] = [];
-        let hashmap: Map<number, number> = new Map(Array.from({ length: 9 }, (_, i) => [i + 1, 1]));
-        for (let j = 0; j < 9; j++) {
-            const rowEle: HTMLElement = document.getElementById(`${i}${j}`);
-            if ((<HTMLInputElement>(rowEle)).value === '') {
-                arr.push(`${i}${j}`);
-            }
-            else {
-                hashmap.set(Number((<HTMLInputElement>(rowEle)).value), 0);
-            }
-        }
-        const values: number[] = [];
-        for (let i = 1; i < 10; i++) {
-            if (hashmap.get(i) === 1) {
-                values.push(i);
+function findPotentialValues(): boolean {
+    
+    const mapComplement = function (foundVals: Map<number, number>): number[]{
+        return Array.from({ length: 9 }, (_, i) => i).filter(i => !foundVals.has(i));
+    }
+
+    function innerJoinSet(arr1: number[], arr2: number[]): number[]{
+        const inBoth: number[] = [];
+        for(let i = 0; i < arr1.length; i++){
+            if(arr2.includes(arr1[i]) && !(inBoth.includes(arr1[i]))){
+                inBoth.push(arr1[i]);
             }
         }
 
-        for (let i = 0; i < arr.length; i++) {
-            if (potentialValue.get(arr[i]) === undefined) {
-                potentialValue.set(arr[i], values);
+        return inBoth;
+    }
+
+    if(board.length != 9 && board[0].length != 9){
+        return false;
+    }
+
+    for(let i = 0; i < 9; i++){
+        const rowPossibilities: Map<number, number> = new Map(Array.from({ length: 9 }, (_, i) => [i + 1, 1]));
+        const rowUnknowns: number[][] = [];
+        const colPossibilities: Map<number, number> = new Map(Array.from({ length: 9 }, (_, i) => [i + 1, 1]));
+        const colUnknowns: number[][] = [];
+
+        for(let j = 0; j < 9; j++){
+            if(board[i][j] == -1){
+                rowUnknowns.push([i, j]);
             }
-            else {
-                potentialValue.set(arr[i], potentialValue.get(arr[i]).concat(values));
+            else{
+                rowPossibilities.set(board[i][j], 0);
+            }
+
+            if(board[j][i] == -1){
+                colUnknowns.push([j, i]);
+            }
+            else{
+                colPossibilities.set(board[j][i], 0);
+            }
+        }
+
+        for(let i = 0; i < rowUnknowns.length; i++){
+            if(potentialValue.get(JSON.stringify(rowUnknowns[i])) === undefined){
+                potentialValue.set(JSON.stringify(rowUnknowns[i]), mapComplement(rowPossibilities));
+            }
+            else{
+                potentialValue.set(JSON.stringify(rowUnknowns[i]), innerJoinSet(potentialValue.get(JSON.stringify(rowUnknowns[i])), mapComplement(rowPossibilities)));
+            }
+        }
+
+        for(let i = 0; i < colUnknowns.length; i++){
+            if(potentialValue.get(JSON.stringify(colUnknowns[i])) === undefined){
+                potentialValue.set(JSON.stringify(colUnknowns[i]), mapComplement(colPossibilities));
+            }
+            else{
+                potentialValue.set(JSON.stringify(colUnknowns[i]), innerJoinSet(potentialValue.get(JSON.stringify(colUnknowns[i])), mapComplement(colPossibilities)));
             }
         }
 
     }
+    
+    // for (let i = 0; i < 9; i++) {
+    //     const arr: string[] = [];
+    //     let hashmap: Map<number, number> = new Map(Array.from({ length: 9 }, (_, i) => [i + 1, 1]));
+    //     for (let j = 0; j < 9; j++) {
+    //         const rowEle: HTMLElement = document.getElementById(`${i}${j}`);
+    //         if ((<HTMLInputElement>(rowEle)).value === '') {
+    //             arr.push(`${i}${j}`);
+    //         }
+    //         else {
+    //             hashmap.set(Number((<HTMLInputElement>(rowEle)).value), 0);
+    //         }
+    //     }
+    //     const values: number[] = [];
+    //     for (let i = 1; i < 10; i++) {
+    //         if (hashmap.get(i) === 1) {
+    //             values.push(i);
+    //         }
+    //     }
 
-    for (let j = 0; j < 9; j++) {
-        const arr: string[] = [];
-        let hashmap: Map<number, number> = new Map(Array.from({ length: 9 }, (_, i) => [i + 1, 1]));
-        for (let i = 0; i < 9; i++) {
-            const rowEle = document.getElementById(`${i}${j}`);
-            if ((<HTMLInputElement>rowEle).value === '') {
-                arr.push(`${i}${j}`);
-            }
-            else {
-                hashmap.set(Number((<HTMLInputElement>rowEle).value), 0)
-            }
-        }
-        const values: number[] = [];
-        for (let i = 1; i < 10; i++) {
-            if (hashmap.get(i) === 1) {
-                values.push(i);
-            }
-        }
+    //     for (let i = 0; i < arr.length; i++) {
+    //         if (potentialValue.get(arr[i]) === undefined) {
+    //             potentialValue.set(arr[i], values);
+    //         }
+    //         else {
+    //             potentialValue.set(arr[i], potentialValue.get(arr[i]).concat(values));
+    //         }
+    //     }
 
-        for (let i = 0; i < arr.length; i++) {
-            if (potentialValue.get(arr[i]) === undefined) {
-                potentialValue.set(arr[i], values);
-            }
-            else {
-                potentialValue.set(arr[i], Array.from(new Set(potentialValue.get(arr[i]).concat(values))));
-            }
-        }
+    // }
 
-    }
+    // for (let j = 0; j < 9; j++) {
+    //     const arr: string[] = [];
+    //     let hashmap: Map<number, number> = new Map(Array.from({ length: 9 }, (_, i) => [i + 1, 1]));
+    //     for (let i = 0; i < 9; i++) {
+    //         const rowEle = document.getElementById(`${i}${j}`);
+    //         if ((<HTMLInputElement>rowEle).value === '') {
+    //             arr.push(`${i}${j}`);
+    //         }
+    //         else {
+    //             hashmap.set(Number((<HTMLInputElement>rowEle).value), 0)
+    //         }
+    //     }
+    //     const values: number[] = [];
+    //     for (let i = 1; i < 10; i++) {
+    //         if (hashmap.get(i) === 1) {
+    //             values.push(i);
+    //         }
+    //     }
+
+    //     for (let i = 0; i < arr.length; i++) {
+    //         if (potentialValue.get(arr[i]) === undefined) {
+    //             potentialValue.set(arr[i], values);
+    //         }
+    //         else {
+    //             potentialValue.set(arr[i], Array.from(new Set(potentialValue.get(arr[i]).concat(values))));
+    //         }
+    //     }
+    // }
 }
 
